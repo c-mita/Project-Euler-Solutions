ed8abc5c69b406390e51c4bcbde2114d19261850
Charles Mita <mita.charles@gmail.com>
2015-11-07 15:51:33 +0000

    Further improvements to 096

--- src/096.cpp
+++ src/096.cpp
@@ -8,6 +8,7 @@
 #define LENGTH 9
 #define SIZE 81
 #define PEERS 20
+#define UNITS 27
 #define INPUT_FILE "096.txt"
 
 /*
@@ -21,7 +22,7 @@
  * When eliminating options from peers, check for cells that only have a single possible value
  * and allow this set/check process to recurse.
  *
- * Project Euler grids can be solved in ~20ms (3.4Ghz i5)
+ * Project Euler grids can be solved in ~15ms (3.4Ghz i5)
  */
 
 
@@ -36,6 +37,78 @@ static int example[SIZE] = {
         8, 0, 0, 2, 0, 3, 0, 0, 9,
         0, 0, 5, 0, 1, 0, 3, 0, 0 };
 
+
+static int PEER_SET[SIZE * PEERS] = {0};
+static int UNIT_SET[UNITS * LENGTH] = {0};
+
+void calcUnitArr( int* out ) {
+    //rows
+    for ( int i = 0; i < LENGTH; i++ ) {
+        for ( int j = 0; j < LENGTH; j++ ) {
+            *(out++) = j + i * LENGTH;
+        }
+    }
+    //columns
+    for ( int i = 0; i < LENGTH; i++ ) {
+        for ( int j = 0; j < LENGTH; j++ ) {
+            *(out++) = i + j * LENGTH;
+        }
+    }
+    for ( int x = 0; x < LENGTH; x += 3 ) {
+        for ( int y = 0; y < LENGTH; y += 3 ) {
+            for ( int i = x; i < x + 3; i++ ) {
+                for ( int j = y; j < y + 3; j++ ) {
+                    *(out++) = i + j * LENGTH;
+                }
+            }
+        }
+    }
+}
+
+int* getPeerSet( int idx ) {
+    return &PEER_SET[ idx * PEERS ];
+}
+
+int* getPeerSet( int x, int y ) {
+    return getPeerSet( x + y * LENGTH );
+}
+
+void calcPeers( int x, int y, int* out ) {
+    //Row
+    for ( int i = 0; i < x; i++ ) {
+        *(out++) = i + y * LENGTH;
+    }
+    for ( int i = x + 1; i < LENGTH; i++ ) {
+        *(out++) = i + y * LENGTH;
+    }
+
+    //Column
+    for ( int i = 0; i < y; i++ ) {
+        *(out++) = x + i * LENGTH;
+    }
+    for ( int i = y + 1; i < LENGTH; i++ ) {
+        *(out++) = x + i * LENGTH;
+    }
+
+    //Mark the cells in a box we "don't" want to add - avoids ugly switch statement
+    bool done_box[9] = {false};
+    for ( int i = 0; i < 3; i++ ) {
+        done_box[x % 3 + i * 3] = true;
+        done_box[i + (y % 3) * 3] = true;
+    }
+
+    //Box
+   int box_x = (x / 3) * 3;
+   int box_y = (y / 3) * 3;
+   for ( int i = box_x, k = 0; k < 3; i++, k++ ) {
+       for ( int j = box_y, l = 0; l < 3; j++, l++ ) {
+           if ( !done_box[k + l * 3] ) {
+               *(out++) = i + j * LENGTH;
+           }
+       }
+   }
+
+}
 
 struct Cell {
     int value;
@@ -53,11 +126,15 @@ public:
             cells[i].possibilities[0] = 0;
             cells[i].num_possibilities = 9;
         }
-        for ( int i = 0; i < LENGTH; i++ ) {
-            for ( int j = 0; j < LENGTH; j++ ) {
-                int value = cells[i + j * LENGTH].value;
-                if ( value ) {
-                    setCellValue( i, j, value );
+        for (int idx = 0; idx < SIZE; idx++ ) {
+            int value = cells[idx].value;
+            if ( value ) {
+                //calling setCellValue can result in inconsistencies due to the elimination process
+                for ( int i = idx * PEERS; i < ( idx + 1 ) * PEERS; i++ ) {
+                    if ( cells[PEER_SET[i]].possibilities[value] ) {
+                        cells[PEER_SET[i]].possibilities[value] = 0;
+                        cells[PEER_SET[i]].num_possibilities--;
+                    }
                 }
             }
         }
@@ -67,82 +144,70 @@ public:
     //*or* we'd preallocate a load of Grid objects and reference those in our recursion (and maybe memset over them...)
     Cell cells[SIZE];
 
-    void setCellValue( int x, int y, int value ) {
+    void setCellValue( int idx, int value ) {
         if ( value == 0 ) {
             return;
         }
-        int idx = x + y * LENGTH;
         cells[idx].value = value;
         memset( cells[idx].possibilities, 0, sizeof(cells[idx].possibilities) );
         cells[idx].num_possibilities = 0;
-        Cell* peers[PEERS];
-        getPeers( x, y, peers );
-        for ( int k = 0; k < PEERS; k++ ) {
-            if ( peers[k]->possibilities[value] ) {
-                peers[k]->possibilities[value] = 0;
-                peers[k]->num_possibilities--;
+
+        Cell* peer;
+        for ( int i = idx * PEERS; i < (idx + 1) * PEERS ; i++ ) {
+            peer = &cells[ PEER_SET[i] ];
+            if ( peer->possibilities[value] ) {
+                peer->possibilities[value] = 0;
+                peer->num_possibilities--;
             }
         }
         clean();
     }
 
+    void setCellValue( int x, int y, int value ) {
+        setCellValue( x + y * LENGTH, value );
+    }
+
+    //Eliminate "obvious" possibilities
+    //Eliminates single possibilities in cells and single possible locations across units
+    //Running *both* optimisations over just one makes no difference for the Project Euler problem set,
+    //but for the harder problems out there it can make a significant difference
     void clean() {
+        //Eliminate values that are the only possibility for that cell
         for ( int i = 0; i < SIZE; i++ ) {
             if ( cells[i].num_possibilities == 1 ) {
                 int value = 0;
-                for ( int k = 1; k < 10; k++ ) {
+                for ( int k = 1; k < LENGTH + 1; k++ ) {
                     if ( cells[i].possibilities[k] ) {
                         value = k;
                         break;
                     }
                 }
-                setCellValue( i % LENGTH, i / LENGTH, value );
+                setCellValue( i, value );
             }
         }
-    }
-
-    //get all peers of specified cell - of which there are 20
-    //use zero-based indexing
-    void getPeers( int x, int y, Cell* out[] ) {
-        //Row
-        for ( int i = 0; i < x; i++ ) {
-            *(out++) = &(cells[i + y * LENGTH]);
+        //Eliminate values that can only go in a single location within a Unit
+        for ( int i = 0; i < UNITS; i++ ) {
+            int* unit = &UNIT_SET[ i * LENGTH ];
+            for ( int p = 1; p < LENGTH + 1; p++ ) {
+                int count = 0;
+                int pos = 0;
+                for ( int k = 0; k < LENGTH; k++ ) {
+                    if ( cells[ unit[k] ].possibilities[p] ) {
+                        count++;
+                        pos = k;
+                    }
+                }
+                if ( count == 1 ) {
+                    setCellValue( unit[pos], p );
+                }
+            }
         }
-        for ( int i = x + 1; i < LENGTH; i++ ) {
-            *(out++) = &(cells[i + y * LENGTH]);
-        }
-
-        //Column
-        for ( int i = 0; i < y; i++ ) {
-            *(out++) = &(cells[x + i * LENGTH]);
-        }
-        for ( int i = y + 1; i < LENGTH; i++ ) {
-            *(out++) = &(cells[x + i * LENGTH]);
-        }
-
-        //Mark the cells in a box we "don't" want to add - avoids ugly switch statement
-        bool done_box[9] = {false};
-        for ( int i = 0; i < 3; i++ ) {
-            done_box[x % 3 + i * 3] = true;
-            done_box[i + (y % 3) * 3] = true;
-        }
-
-        //Box
-       int box_x = (x / 3) * 3;
-       int box_y = (y / 3) * 3;
-       for ( int i = box_x, k = 0; k < 3; i++, k++ ) {
-           for ( int j = box_y, l = 0; l < 3; j++, l++ ) {
-               if ( !done_box[k + l * 3] ) {
-                   *(out++) = &(cells[i + j * LENGTH]);
-               }
-           }
-       }
     }
 
     //Find a cell with no given value
     bool findUnknown( int* x, int* y, Cell** cell ) {
         //Ideally should return cells with fewer possibilities first
-        //But it doesn't really matter
+        //But a linear search on unsorted values is too slow and it doesn't really matter
         for ( int i = 0; i < SIZE; i++ ) {
             if ( cells[i].value == 0 ) {
                 *x = i % LENGTH;
@@ -215,6 +280,14 @@ parseGrids( const char* path ) {
 int main( int argc, char* argv[] ) {
     auto startTime = std::chrono::steady_clock::now();
 
+    calcUnitArr( UNIT_SET );
+
+    for ( int i = 0; i < LENGTH; i++ ) {
+        for ( int j = 0; j < LENGTH; j++ ) {
+            calcPeers(i, j, &PEER_SET[PEERS * (i + j * LENGTH)]);
+        }
+    }
+
     const char* filePath;
     if ( argc <= 1 ) {
         filePath = INPUT_FILE;
@@ -231,12 +304,12 @@ int main( int argc, char* argv[] ) {
         if ( !solve( g, &out ) ) {
             std::cout << "FAILED" << std::endl;
             std::cout << g.str() << std::endl;
-            throw;
         } else {
             auto gridRunTime = std::chrono::steady_clock::now() - gridTime;
             double timeTaken = std::chrono::duration<double, std::milli> (gridRunTime).count();
-            if ( timeTaken > 1000. ) {
+            if ( timeTaken > 100. ) {
                 std::cout << "This took " << timeTaken << "ms" << std::endl;
+                std::cout << g.str() << std::endl;
                 std::cout << out.str() << std::endl;
             }
         }
