b995cf80a26a6d2e823756a9187565e016122d45
Charles Mita <mita.charles@gmail.com>
2018-09-29 18:50:12 +0100

    Solve problem 66.
    Requires libgmp for big integers.

---
+++ src/066.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <chrono>
+#include <tuple>
+#include <math.h>
+#include <stdexcept>
+#include <gmpxx.h>
+
+/*
+ * http://www.isibang.ac.in/~sury/chakravala.pdf
+ */
+
+typedef mpz_class bigint_t;
+
+std::tuple<bigint_t, bigint_t>
+chak_method(int n) {
+    bigint_t p, q;
+    bigint_t p2, q2;
+    int m, x;
+    double sq_n = sqrt(n);
+    if (((int) sq_n) * ((int) sq_n) == n) {
+        throw std::domain_error("No solution for perfect squares");
+    }
+    p = (int) sq_n;
+    q = 1;
+    m = (int) sq_n * (int) sq_n - n;
+    x = (int) sq_n;
+
+    while (m != 1) {
+        int abs_m = m > 0 ? m : -m;
+        int abs_x = x > 0 ? x : -x;
+        x = (abs_m - abs_x) % abs_m;
+        while (x + abs_m < sq_n) x += abs_m;
+
+        p2 = (p*x + n*q) / abs_m;
+        q2 = (p + q*x) / abs_m;
+        m = (x*x - n) / m;
+        p = p2;
+        q = q2;
+    }
+
+    return std::make_tuple(p, q);
+}
+
+int main() {
+    auto start_time = std::chrono::steady_clock::now();
+
+    int limit = 1000;
+    bigint_t max_x = 0;
+    int max_i = 0;
+    for (int i = 2; i < limit; i++) {
+        int rt = (int) sqrt(i);
+        if (rt * rt == i) continue;
+        std::tuple<bigint_t, bigint_t> res = chak_method(i);
+        bigint_t x  = std::get<0>(res);
+        if (x > max_x) {
+            max_x = x;
+            max_i = i;
+        }
+    }
+    std::cout << max_i << std::endl;
+    std::cout << max_x.get_str() << std::endl;
+
+    auto end_time = std::chrono::steady_clock::now();
+    auto run_time = end_time - start_time;
+    std::cout << "Time: " << std::chrono::duration<double, std::milli> (run_time).count() << " ms" << std::endl;
+    return 0;
+
+}
